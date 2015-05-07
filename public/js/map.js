ced67ae7b7454369236c831bc0a5b6d510187775
Matt Smith <tangohead@gmail.com>
2015-05-07 11:58:23 +0100

    Waypoints added and working, elevation calculation for the returned route also added

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,6 @@
 var dir_disp = null;
 var markers = null;
+var elev_srv = null;
 $(document).ready(function(){
 	function initialize() {
         var mapOptions = {
@@ -27,7 +28,6 @@ $(document).ready(function(){
 
 
 function create_marker(map, latLng){
-    console.log("marker created");
 	var marker = new google.maps.Marker({
     		position: latLng,
     		map:map,
@@ -37,46 +37,107 @@ function create_marker(map, latLng){
 		console.log(marker);
  	})
 	markers.push(marker);
+    calculate_route(markers);
+}
 
+function calculate_route(markers)
+{
     if(markers.length > 1)
     {
-    	var dir_srv = new google.maps.DirectionsService();
-        console.log("Markers currently set");
-        console.log(markers);
+        var dir_srv = new google.maps.DirectionsService();
 
-    	//Create request
-        console.log(markers[0].getPosition());
-    	dir_req = {
-    		origin : markers[0].getPosition(),
-    		destination : markers[markers.length - 1].getPosition(),
-    		travelMode : google.maps.TravelMode.BICYCLING,
-    		unitSystem : google.maps.UnitSystem.Metric,
-   		};
+        //Create request
+        dir_req = {
+            origin : markers[0].getPosition(),
+            destination : markers[markers.length - 1].getPosition(),
+            travelMode : google.maps.TravelMode.BICYCLING,
+            unitSystem : google.maps.UnitSystem.Metric,
+        };
 
         //If we've got waypoints, then we need to put the waypoints in
         if(markers.length > 2)
         {
-            console.log("Calculating waypoints");
             var waypoints = new Array();
 
             for(var i=1; i < markers.length-1; i++)
             {
-                console.log(markers[i].getPosition());
-                waypoints.push(new google.maps.DirectionsWaypoint({
+                waypoints.push({
                     location: markers[i].getPosition(),
                     stopover: false
-                })
-                );
+                });
             }
-            console.log(waypoints);
             dir_req.waypoints = waypoints;
         }
 
-   		dir_srv.route(dir_req, function(result, status){
-   			if(status == google.maps.DirectionsStatus.OK){
-   				console.log(result);
-   				dir_disp.setDirections(result);
-   			}
-   		});
+        dir_srv.route(dir_req, function(result, status){
+            switch(status)
+            {
+            case google.maps.DirectionsStatus.OK:
+                dir_disp.setDirections(result);
+                retrieve_elevation(result.routes[0].overview_path);
+                break;
+             case google.maps.DirectionsStatus.MAX_WAYPOINTS_EXCEEDED:
+                console.error("Too many waypoints specified for directions");
+                break;
+            case google.maps.DirectionsStatus.NOT_FOUND:
+                console.error("One of the origin, destination or waypoints could not be found");
+                break;
+            case google.maps.DirectionsStatus.ZERO_RESULTS:
+                console.error("No route could be found between origin and destination");
+                break;
+            case google.maps.DirectionsStatus.INVALID_REQUEST:
+                console.error("Invalid request to Directions Service");
+                break;
+            case google.maps.DirectionsStatus.REQUEST_DENIED:
+                console.error("Request denied to Directions Service");
+                break;
+            case google.maps.DirectionsStatus.OVER_QUERY_LIMIT:
+                console.error("Too many requests to Directions Service");
+                break;
+            case google.maps.DirectionsStatus.UNKNOWN_ERROR:
+                console.error("Unknown error from Directions Service");
+                break;
+            default:
+                console.error("Internal error on directions");
+                break;
+            }
+
+        });
     }
+}
+
+function retrieve_elevation(path)
+{
+    elev_srv = new google.maps.ElevationService();
+    elev_srv.getElevationAlongPath({
+        path: path,
+        samples: path.length
+    }, function(results, status){
+        switch(status)
+        {
+        case google.maps.ElevationStatus.OK:
+            draw_elevation_profile(results);
+            break;
+        case google.maps.ElevationStatus.INVALID_REQUEST:
+            console.error("Invalid request to Elevation Service");
+            break;
+        case google.maps.ElevationStatus.REQUEST_DENIED:
+            console.error("Request denied to Elevation Service");
+            break;
+        case google.maps.ElevationStatus.OVER_QUERY_LIMIT:
+            console.error("Too many requests to Elevation Service");
+            break;
+        case google.maps.ElevationStatus.UNKNOWN_ERROR:
+            console.error("Unknown error from Elevation Service");
+            break;
+        default:
+            console.error("Internal error on elevation");
+            break;
+        }
+    });
+}
+
+function draw_elevation_profile(elevation_profile)
+{
+    console.log(elevation_profile);
 }
